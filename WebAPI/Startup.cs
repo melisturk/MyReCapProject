54750b1414b70df91dcfeecebb1af31a80cd2a22
Melis Türk <melis-trk@outlook.com>
2021-02-23 23:58:23 +0300

    Autofac desteği eklendi

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -30,8 +30,8 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<ICarService, CarManager>();
-            services.AddSingleton<ICarDal, EfCarDal>();
+            //services.AddSingleton<ICarService, CarManager>();
+            //services.AddSingleton<ICarDal, EfCarDal>();
 
         }
 
